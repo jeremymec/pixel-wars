d1c8a7c8ce47df5cc56b1f64f3cc5b8ac7e19fb1
Jeremy Mechen <jeremymechen1997@gmail.com>
2017-06-26 09:56:13 +1200

    Initial Commit

---
+++ Assets/Scripts/fireballScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireballScript : MonoBehaviour {
+
+    bool moving = false;
+    Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void setTarget(Vector3 target)
+    {
+        this.target = target;
+        this.moving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.Equals(target))
+        {
+            this.moving = false;
+            Debug.Log("Boom!");
+            Destroy(this.gameObject);
+        }
+
+        if (moving)
+        {
+            float step = Time.deltaTime * 5f;
+
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+        }
+
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    enum States { NORMAL, IMMOBILE }
+
+    States state = States.NORMAL;
+    float speed = 1.2f;
+    GameObject fireball;
+
+	// Use this for initialization
+	void Start () {
+        fireball = Resources.Load("fireball") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (state == 0)
+        {
+            // Movement Logic
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.position += Vector3.up * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.position += Vector3.down * speed * Time.deltaTime;
+            }
+        }
+
+
+        // Casting Fireball
+        if (Input.GetKeyDown(KeyCode.F)){
+            StartCoroutine("castFireball");
+        }
+
+    }
+
+    IEnumerator castFireball()
+    {
+        this.state = States.IMMOBILE;
+        GameObject fireball = Instantiate(this.fireball) as GameObject;
+        Physics2D.IgnoreLayerCollision(8, 9);
+
+        fireball.transform.position = transform.position;
+        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos.ToString());
+
+        Vector3 initialMove = fireball.transform.rotation * new Vector3(0.1f, 0.1f, 0);
+        fireball.transform.position += 5f * initialMove;
+
+        Vector3 deltaToTarget = mousePos - fireball.transform.position;
+        float angle = (Mathf.Atan2(deltaToTarget.y, deltaToTarget.x) * Mathf.Rad2Deg) - 180;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        fireball.transform.rotation = Quaternion.Slerp(fireball.transform.rotation, q, 2f);
+
+        yield return new WaitForSeconds(1f);
+
+        fireball.SendMessage("setTarget", mousePos);
+        this.state = States.NORMAL;
+    }
+
+
+}
